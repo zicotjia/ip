13ad7c397f0e9f4079cbaf6e1f9dd23bdf721203
zicot <zicotjia@gmail.com>
2022-08-17 20:07:11 +0800

    small fixed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -72,6 +72,11 @@ public class Duke {
 
             switch (command) {
                 case "list":
+                    if (!myScanner.nextLine().isBlank()) {
+                        System.out.println("Too many Arguments");
+                        break;
+                    }
+
                     System.out.println(SEPARATOR);
                     duke.read();
                     break;
@@ -84,63 +89,82 @@ public class Duke {
                     }
 
                     System.out.println(SEPARATOR);
+
                     if (index <= 0) {
                         System.out.println("index cannot be zero or negative");
                         break;
                     } else {
                         duke.mark(index - 1);
                     }
+
                     break;
                 case "unmark":
                     int index1 = myScanner.nextInt();
+
                     if (!myScanner.nextLine().isBlank()) {
                         System.out.println("Too many Arguments");
                         break;
                     }
 
                     System.out.println(SEPARATOR);
+
                     if (index1 < 0) {
                         System.out.println("index cannot be negative");
                         break;
                     } else {
                         duke.unMark(index1 - 1);
                     }
+
                     break;
                 case "deadline":
                     String unParsed = myScanner.nextLine();
                     String[] descriptionAndBy =  unParsed.split("/by", 2);
+
                     if (descriptionAndBy.length != 2) {
                         System.out.println("Invalid Commands");
                         break;
                     }
+
                     System.out.println(SEPARATOR);
+
                     Deadline newDeadLine = new Deadline(descriptionAndBy[0], descriptionAndBy[1]);
                     duke.add(newDeadLine);
+
                     break;
                 case "todo":
                     String description = myScanner.nextLine();
+
                     if (description.isBlank()) {
                         System.out.println("Empty Arguments");
                     }
+
                     System.out.println(SEPARATOR);
+
                     ToDo newToDo = new ToDo(description);
                     duke.add(newToDo);
+
                     break;
                 case "event":
                     String unParsed1 = myScanner.nextLine();
-                    String[] descriptionAndBy1 =  unParsed1.split("/T", 2);
+                    String[] descriptionAndBy1 =  unParsed1.split("/at", 2);
+
                     if (descriptionAndBy1.length != 2) {
                         System.out.println("Invalid Commands");
                         break;
                     }
+
                     System.out.println(SEPARATOR);
+
                     Event newDeadLine1 = new Event(descriptionAndBy1[0], descriptionAndBy1[1]);
                     duke.add(newDeadLine1);
+
                     break;
                 case "bye":
                     System.out.println(SEPARATOR);
+
                     System.out.println("See you later :)");
                     System.exit(0);
+
                     break;
                 default:
                     System.out.println("Unknown Command");
